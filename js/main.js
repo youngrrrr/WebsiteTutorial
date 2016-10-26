eb90b22e2e7c78298e56793a362f96477ec83bd7
youngrrrr <young-rae_kim@brown.edu>
2016-10-25 22:55:15 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-const MAX_FONT_SIZE = 50;
+const MAX_COLOR_VAL = 255;
 
 $("#pic").on("click", function() {
-  var randomFontSize = Math.floor(Math.random() * MAX_FONT_SIZE);
-  $(".my-font-size").css("font-size", randomFontSize + "px");
+  var randomColorArr = getRandomColorArray();
+  $(".my-font-color").css("color", "rgb(" + rgbArray[0] + "," + rgbArray[1] + "," + rgbArray[2] + ")");
 });
+
+function getRandomColorArray() {
+  var r = Math.floor(Math.random() * MAX_COLOR_VAL);
+  var g = Math.floor(Math.random() * MAX_COLOR_VAL);
+  var b = Math.floor(Math.random() * MAX_COLOR_VAL);
+
+  return [r, g, b];
+}
